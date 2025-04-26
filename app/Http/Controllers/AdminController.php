afd4ac995e40741f1881171b2200355807cc08c2
lili5777 <zailyanzali@gmail.com>
2025-04-26 22:20:10 +0800

    dsaad

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Belajar;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -17,12 +20,29 @@ class AdminController extends Controller
 
     public function beranda()
     {
-        return view('dashboard');
+        $tweet=Belajar::all();
+        $user=User::all();
+        return view('dashboard',compact('tweet','user'));
     }
 
     public function profil()
     {
         return view('profil');
+    }
+
+    public function posting(Request $request){
+        $request->validate([
+            'postingan' => 'required',
+        ]);
+
+        $post = new Belajar();
+        $post->id_user = auth()->id(); 
+        $post->topik = $request->postingan;
+        $post->jam = Carbon::now()->format('H:i'); 
+        $post->tanggal = Carbon::now()->format('Y-m-d'); 
+        $post->save(); 
+
+        return back()->with('success', 'Postingan berhasil dikirim!');
     }
     
     
